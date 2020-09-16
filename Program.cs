cf9bd34e65fb36f1cf4947f6d8254a50c41fa477
Stylebykat <Stylebykat@LAPTOP-OJ9JTFP6>
2020-09-16 01:14:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ Author: Katera Jefferson
+ Date: 9/15/2020
+ */
+
+
+using System;
+
+namespace I_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an Integer value between 1 and 25 to execute an iterative statement: ");
+            try
+            {
+                string input = Console.ReadLine();
+                int value_of_input = int.Parse(input);
+                if ((value_of_input > 0) && (value_of_input <= 5))
+                {
+                    Console.WriteLine("Executing a For Loop!");
+                    Console.WriteLine("The For Loop will iterate " + value_of_input.ToString() + "times.");
+                    for (int i = 0; i < value_of_input; i++)
+                    {
+                        Console.WriteLine("You have entered" + value_of_input.ToString() + "This is the current integer value in the loop: " + value_of_input.ToString());
+                    }
+                    Console.WriteLine("Press any key to exit the program...");
+                    Console.ReadKey(true);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
